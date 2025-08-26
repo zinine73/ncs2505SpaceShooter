f48859fc956eb222bb6de6972b03f68a81f3f0f4
zinine73 <zinine@naver.com>
2025-08-26 15:50:14 +0900

    InputManager

--- Assets/02_Scripts/PlayerCtrl.cs
+++ Assets/02_Scripts/PlayerCtrl.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // component cash
+    Transform tr;
     void Start()
     {
-        
+        // Get Component
+        //tr = this.gameObject.GetComponent<Transform>();
+        //tr = GetComponent("Transform") as Transform;
+        //tr = (Transform)GetComponent(typeof(Transform));
+        tr = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Debug.Log($"h={h}");
+        Debug.Log($"v={v}");
+        // Transform.position
+        //transform.position += new Vector3(0, 0, 1);
+        // normalized vector
+        tr.position += Vector3.forward * 1;
     }
 }
