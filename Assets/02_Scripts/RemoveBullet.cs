72355ad7c6d4a94d9787a47710a92a36881162d0
zinine73 <zinine@naver.com>
2025-09-09 13:53:13 +0900

    Quaternion

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
+
     // 충돌이 시작할 때 발생하는 이벤트
     void OnCollisionEnter(Collision collision)
     {
@@ -12,6 +14,12 @@ public class RemoveBullet : MonoBehaviour
         //if (collision.gameObject.tag.Equals("BULLET"))
         if (collision.collider.CompareTag("BULLET"))
         {
+            ContactPoint cp = collision.GetContact(0);
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+            GameObject spark = Instantiate(sparkEffect,
+                collision.transform.position,
+                rot); //Quaternion.identity);
+            Destroy(spark, 0.5f);
             Destroy(collision.gameObject);
         }
     }
