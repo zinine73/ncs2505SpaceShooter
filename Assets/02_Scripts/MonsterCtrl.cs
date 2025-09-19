87319bd8a65359e081ef7e693340a8c8c732ba32
zinine73 <zinine@naver.com>
2025-09-19 13:50:35 +0900

    StartCoroutine

--- Assets/02_Scripts/MonsterCtrl.cs
+++ Assets/02_Scripts/MonsterCtrl.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 using UnityEngine.AI;
 public class MonsterCtrl : MonoBehaviour
 {
+    const float TIME_WAIT = 0.3f;
+
+    // 몬스터의 상태 정보
+    public enum State { IDLE, TRACE, ATTACK, DIE }
+    public State state = State.IDLE;
+    public float traceDist = 10.0f;
+    public float attackDist = 2.0f;
+    public bool isDie = false;
+
     Transform monsterTr;
     Transform playerTr;
     NavMeshAgent agent;
+    Animator anim;
     void Start()
     {
         // monsterTr
@@ -17,8 +27,73 @@ public class MonsterCtrl : MonoBehaviour
             .GetComponent<Transform>();
         // agent
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         // start trace
         //agent.destination = playerTr.position;
-        agent.SetDestination(playerTr.position);
+        //agent.SetDestination(playerTr.position);
+
+        // 몬스터의 상태를 체크하는 코루틴
+        StartCoroutine(CheckMonsterState());
+        // 상태에 따라 몬스터의 행동을 수행하는 코루틴
+        StartCoroutine(MonsterAction());
+    }
+
+    IEnumerator CheckMonsterState()
+    {
+        while (!isDie)
+        {
+            yield return new WaitForSeconds(TIME_WAIT);
+            float distance = Vector3.Distance(playerTr.position, monsterTr.position);
+            if (distance <= attackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+        }
+    }
+
+    IEnumerator MonsterAction()
+    {
+        while (!isDie)
+        {
+            switch (state)
+            {
+                case State.IDLE:
+                    agent.isStopped = true;
+                    anim.SetBool("IsTrace", false);
+                    break;
+                case State.TRACE:
+                    agent.SetDestination(playerTr.position);
+                    agent.isStopped = false;
+                    anim.SetBool("IsTrace", true);
+                    break;
+                case State.ATTACK:
+                    break;
+                case State.DIE:
+                    break;
+            }
+            yield return new WaitForSeconds(TIME_WAIT);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (state == State.TRACE)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, traceDist);
+        }
+        if (state == State.ATTACK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackDist);
+        }
     }
 }
